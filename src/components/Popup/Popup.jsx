db643123139598d2ce8ec3ef4d5d19e3f5f8b585
daysg <days.grace2015@yandex.ru>
2022-08-04 12:48:42 +0300

    feat: opening a modal window

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -3,7 +3,7 @@ import styles from './Popup.module.scss'
 
 const Popup = ({active, setActive}) => {
     return (
-        <div className={active ? `${styles.popup.active}` : `${styles.popup}`} onClick={() => setActive(false)}>
+        active ? <div className={styles.popup} onClick={() => setActive(false)}>
             <div className={styles.popup__content} onClick={(e) => e.stopPropagation()}>
                 <div className={styles.popup__close} onClick={() => setActive(false)}></div>
                 <label className={styles.popup__input}>
@@ -12,7 +12,7 @@ const Popup = ({active, setActive}) => {
                 <ul className={styles.popup__results}></ul>
                 <button type="button" className={styles.popup__button}>OK</button>
             </div>
-        </div>
+        </div> : null
     )
 }
 export default Popup
