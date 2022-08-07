26f2ae7064bf73f4911c0e068e2ddae44b19ab42
daysg <days.grace2015@yandex.ru>
2022-08-07 15:55:29 +0300

    refactor: style code

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,5 @@
 import countryApi from "./countryApi";
 import forecastApi from "./forecastApi";
 import weatherTodayApi from "./weatherTodayApi";
-import api from "./api";
-import geolocation from "./geolocation";
 
-export {countryApi, forecastApi, weatherTodayApi, api, geolocation}
+export {countryApi, forecastApi, weatherTodayApi}
