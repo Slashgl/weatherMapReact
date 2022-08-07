69f65d1fac42328d5fafb88ec46f7ba32a116b78
daysg <days.grace2015@yandex.ru>
2022-08-07 17:59:32 +0300

    fix: re-uploading the project

--- src/components/popup/index.jsx
+++ src/components/popup/index.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
-import {countryApi} from "services";
+import {countryApi} from 'services';
 import styles from './styles.module.scss'
 
 const Popup = ({active, setActive}) => {
     const api = countryApi()
 
     return (
-        active ? <div className={styles.popup} onClick={() => setActive(false)}>
-            <div className={styles.popup__content} onClick={(e) => e.stopPropagation()}>
-                <div className={styles.popup__close} onClick={() => setActive(false)}></div>
-                <label className={styles.popup__input}>
-                    <input className={styles.popup__search} type='text'/>
-                </label>
-                <ul className={styles.popup__results}>
-                    {api && api.map(el => {
-                        return (
-                            <li key={el.city} className={styles.popup__item}>
-                                <div className={styles.popup__name}>{el.city}</div>
-                                <div className={styles.popup__country}>{el.state}</div>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <button type='button' className={styles.popup__button}>OK</button>
+        active ?
+            <div className={styles.popup} onClick={() => setActive(false)}>
+                <div className={styles.popup__content} onClick={(e) => e.stopPropagation()}>
+                    <div className={styles.popup__close} onClick={() => setActive(false)}></div>
+                    <label className={styles.popup__input}>
+                        <input className={styles.popup__search} type='text'/>
+                    </label>
+                    <ul className={styles.popup__results}>
+                        {api && api.map(el => {
+                            return (
+                                <li key={el.city} className={styles.popup__item}>
+                                    <div className={styles.popup__name}>{el.city}</div>
+                                    <div className={styles.popup__country}>{el.state}</div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <button type='button' className={styles.popup__button}>OK</button>
+                </div>
             </div>
-        </div> : null
+            : null
     )
 }
 export default Popup
