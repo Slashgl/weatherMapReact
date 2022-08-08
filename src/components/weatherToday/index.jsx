9f62475ac848e1b285d39a56d2e541c5542b5c66
daysg <days.grace2015@yandex.ru>
2022-08-08 14:06:32 +0300

    refactor: added local images

--- src/components/weatherToday/index.jsx
+++ src/components/weatherToday/index.jsx
@@ -10,11 +10,12 @@ const WeatherToday = () => {
             <div className={styles.today__title}>Cloudy conditions from 1AM-9AM, with
                 showers expected at 9AM.</div>
             <ul className={styles.today__items}>
-                {api && api.map(el => {
+                {api &&
+                    api.map(el => {
                     return (
                         <li key={el.time} className={styles.today__item}>
                             <div className={styles.today__time}>{el.time}</div>
-                            <div className={styles.today__img}><img width='20px' height='20px' src='https://img2.freepng.ru/20180626/ipe/kisspng-computer-icons-cloud-cover-meteorology-rain-day-time-5b32197e96f184.0483834715300099826183.jpg' alt='img'/></div>
+                            <div className={styles.today__img}><img src={el.icon} alt='img'/></div>
                             <div className={styles.today__degrees}>{`${el.degrees}`}&deg;</div>
                         </li>
                     )
