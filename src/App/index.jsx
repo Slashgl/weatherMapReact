acad27c23232077b902e5a210891646a8a35144d
daysg <days.grace2015@yandex.ru>
2022-08-10 17:21:30 +0300

    Style: Adjust styles

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,32 +1,36 @@
 import React, {useState} from 'react'
 import {AsideBar, Forecast, Popup, Header, WeatherBoards, WeatherToday} from 'components'
-import styles from './styles.module.scss'
 import {Hamburger} from "components"
 import {Context} from "../components/context";
+import styles from './styles.module.scss'
+
 
 const App = () => {
     const [modalActive, setModalActive] = useState(false)
     const [switchingModal, setSwitchingModal] = useState(false)
-    console.log(switchingModal)
+
     return (
-        <Context.Provider value={{switchingModal, setSwitchingModal}}>
-            <div className={styles.App}>
-                <div>
-                    <Header/>
-                    <div className={styles.table}>
-                        <Forecast/>
-                        <div>
-                            <WeatherToday/>
-                            <WeatherBoards/>
+        <>
+
+            <Context.Provider value={{switchingModal, setSwitchingModal, setModalActive}}>
+                <div className={styles.App}>
+                    <div>
+                        <Header/>
+                        <div className={styles.table}>
+                            <Forecast/>
+                            <div>
+                                <WeatherToday/>
+                                <WeatherBoards/>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <Hamburger/>
-                <AsideBar setModalActive={setModalActive}/>
-                <Popup active={modalActive} setActive={setModalActive}/>
-            </div>
-        </Context.Provider>
+                    <Hamburger/>
+                    <AsideBar setModalActive={setModalActive}/>
+                    <Popup active={modalActive} setActive={setModalActive}/>
+                </div>
+            </Context.Provider>
+        </>
     )
 }
 
