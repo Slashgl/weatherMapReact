bfc22b027f9daef40e55fc402b860451d7102fd5
daysg <days.grace2015@yandex.ru>
2022-08-27 21:29:44 +0300

    Style: install prettier and edit files

--- src/components/mobileInput/closeButton/index.js
+++ src/components/mobileInput/closeButton/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Button } from '@mui/material'
-import styles from './styles.module.scss'
 
 const CloseButton = ({ setIsHidden, setPanel }) => {
     return (
         <Button
             style={{ height: '45px', marginLeft: '25px' }}
-            className={styles.close}
             variant="outlined"
             onClick={() => {
                 setIsHidden(false)
