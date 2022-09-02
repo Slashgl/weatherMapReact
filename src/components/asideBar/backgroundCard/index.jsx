5f57caa2ee110c57a1403dc9a11545ebd3b1b155
daysg <days.grace2015@yandex.ru>
2022-09-02 21:57:06 +0300

    Style: fixed the width of the card on the mobile phone

--- src/components/asideBar/backgroundCard/index.jsx
+++ src/components/asideBar/backgroundCard/index.jsx
@@ -4,9 +4,13 @@ import styles from './styles.module.scss'
 
 const BackgroundCard = ({ city }) => {
     return (
-        <video className={styles.background} width='100%' height='100%' muted loop autoPlay>
-            <source src={changeBackground(city)}/>
-        </video>
+        <>
+            <video className={styles.background} width='100%' height='100%' poster={changeBackground(city)}>
+            </video>
+            <video className={styles.back} width='100%' height='100%' autoPlay muted loop>
+                <source className={styles.source} src={changeBackground(city)}/>
+            </video>
+        </>
     )
 }
 
